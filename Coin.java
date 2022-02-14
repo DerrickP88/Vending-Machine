778998e4288f9d36ee4abf5cdc5291742ce87b19
Derrick Panciera <soichiroikari@gmail.com>
2022-02-13 18:25:14 -0700

    "added arraylist and bank"

---
+++ Coin.java
@@ -0,0 +1,13 @@
+public class Coin {
+
+    private final double value;
+
+    public Coin(double v){
+        value=v;
+
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class VendingMachine {
@@ -13,8 +14,41 @@ public class VendingMachine {
         int dimes = Integer.parseInt (coinReader.nextLine());
         System.out.println("enter nickels");
         int nickels = Integer.parseInt(coinReader.nextLine());
-        System.out.println("you have $"+total(quarters,dimes, nickels));
+        System.out.println("enter pennies");
+        ArrayList<Coin> bank = new ArrayList<>();
+        for (int x =0;x<quarters;x++){
+            bank.add(new Coin(0.25));
+        }
+
+        for (int x =0;x<dimes;x++){
+            bank.add(new Coin(0.10));
+        }
+
+        for (int x =0;x<nickels;x++){
+            bank.add(new Coin(0.05));
+        }
+
+        int pennies = Integer.parseInt (coinReader.nextLine());
+        for (int x = 0; x<pennies; x++){
+            bank.add(new Coin(0.01));
+        }
+
+
+
+        System.out.println("you have $"+total(bank));
     }
+
+    private static double total(ArrayList<Coin> bank) {
+        for(Coin c : bank){
+            if(c.getValue() != 0.01){
+                total += c.getValue();
+            }else{
+                System.out.println("rejecting penny");
+            }
+        }
+        return total ;
+    }
+
     public static double total(int quarters, int dimes, int nickels){
         total = (quarters *0.25)+(dimes *0.10)+(nickels *0.05);
         return total ;
